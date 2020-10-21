ea0ce4d66802adab984feff100b7318789f6f3d1
Sophie Davies <sophiejeandavies@gmail.com>
2020-10-21 16:08:26 +0100

    Plotted data and renamed summary statistic

--- Analysis.R
+++ Analysis.R
@@ -5,4 +5,7 @@ df <- data.frame(x = rnorm(50, 16, 4), group = rep(c("group1", "group2"), each =
 library(tidyverse)
 
 df %>% group_by(group) %>%
-  summarise(mean = mean(x), n = length(n), sd = sd(x))
+  summarise(mean = mean(x), n = length(n), sdev = sd(x))
+
+df %>% ggplot(aes(x = group, y = x)) +
+  geom_boxplot()
